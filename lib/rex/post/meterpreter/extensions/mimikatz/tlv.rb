3eaca8726d05fc647a017d3ed97569cd0b7faaa2
Meatballs <eat_meatballs@hotmail.co.uk>
2013-04-20 18:47:12 +0100

    Forgot tlv

---
+++ lib/rex/post/meterpreter/extensions/mimikatz/tlv.rb
@@ -0,0 +1,14 @@
+# -*- coding: binary -*-
+module Rex
+module Post
+module Meterpreter
+module Extensions
+module Mimikatz
+
+TLV_TYPE_MIMIKATZ_RESULT	= TLV_META_TYPE_STRING | (TLV_EXTENSIONS + 1)
+
+end
+end
+end
+end
+end
